bbe2fedeaecf84099d35da92e266bf8f7b8e529c
Ron Johnson <ronjohn1414@gmail.com>
2019-09-09 01:43:04 -0400

    Initial commit

---
+++ app/src/main/java/com/example/countries/model/Data.kt
@@ -0,0 +1,2 @@
+package com.example.countries.model
+data class Country(val countryName: String?)
---
+++ app/src/main/java/com/example/countries/viewModel/ListViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.countries.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.countries.model.Country
+
+class ListViewModel: ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        fetchCountries()
+    }
+
+    private fun fetchCountries(){
+        val mockData = listOf(
+            Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH"),
+            Country("CountryI"),
+            Country("CountryJ"),
+            Country("CountryK")
+        )
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+
+    }
+
+}
